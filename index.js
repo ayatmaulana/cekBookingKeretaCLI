8c15c12dc21a02fb5c5a75415be05b3207cf8fc5
Ayat Maulana <me.ayatmaulana@gmail.com>
2018-01-05 21:53:28 +0700

    v2 upgrade

--- index.js
+++ index.js
@@ -9,98 +9,129 @@ const nightmare = Nightmare({
 });
 
 const cheerio = require('cheerio')
-const collect = require('collect.js')
 const commander = require('commander')
+const ora = require('ora')
+const Table = require('cli-table2');
+const chalk = require('chalk');
+const asciify = require('asciify')
 
 var kode_boking 
 // "Z55EJ8"
 
+
+const spinner = ora("Fetching Data ... ")
+spinner.color = "blue"
+spinner.start()
+
 const scrap = async ( code  ) => {
 
   await nightmare
-        .goto('https://tiket.kereta-api.co.id/?_it8tnz=TXc9PQ==&_8dnts=WTJobFkycz0=')
-        .insert("input[name=kode_booking]", code)
-        .click("input[type=submit]")
+        .viewport(1024, 768)
+        .goto('https://kai.id/index.php/home')
+        .click("#accordion .accordion-toggle:nth-child(3) h6")
         .wait(1000)
+        .insert("#kode_booking", code)
+        .wait(1500)
+        .click("#submitcode")
+        .wait(3000)
         .evaluate(() => {
-            return document.querySelector('.itReservationContent').innerHTML
+            let getEl = document.querySelector('#data-input')
+            if( getEl == null )
+                return null
+            
+            return getEl.innerHTML
         })
         .end()
         .then(function (data) {
-            cekBooking.load(data)
+            if( data != null )
+                cekBooking.load(data)
+            else
+                console.log( "data Not found" ); process.exit(1)
         })
         .catch(function (err) {
             console.log(err);
         });
 }
 
-
-
 const cekBooking = {
     html: null,
     passenger: null,
+    general: null,
     load(html) {
         this.html = cheerio.load(html)
         this.parse()
     },
-    tableToObj(tbl) {
-        const $ = this.html
-
-        return tbl.map(function () {
-            return $(this).find('td').map(function () {
-                return $(this).html();
-            }).get();
-        }).get();
-    },
-    parsePassenger() {
-        const $ = this.html
-        let penumpangHtml = $('table:nth-child(2) tr')
-        var tbl = this.tableToObj(penumpangHtml)
-
-        var atbl = tbl.filter((item, i) => {
-            return i >= 6
-        })
-
-        let ch = collect(atbl).chunk(6)
-
-        let passenger = ch.map(item => {
-            return {
-                number: item[0],
-                nama: item[1],
-                no: item[2],
-                tipe: item[3],
-                duduk: item[4],
-                tiket: item[5]
-            }
-        })
-
-        this.passenger = passenger
-    },
-    parse() {
+    parse(){
+        this.parseGeneral()
         this.parsePassenger()
-        console.log(this.parseGeneral())
-
+        this.output()
     },
-    parseGeneral() {
+    parseGeneral(){
         const $ = this.html
-
-        return {
+        this.general = {
             pembayaran: {
-                status_bayar: $('table:nth-child(1) tr:nth-child(3) td:nth-child(3) span').html(),
-                total_bayar: $('table:nth-child(1) tr:nth-child(4) td:nth-child(3) span').html()
+                status_bayar    : $($('.row:not(".head")')[0]).find(".row .columns:nth-child(1) dl dd").text(),
+                total_bayar     : $($('.row:not(".head")')[0]).find(".row .columns:nth-child(2) dl dd").text()
             },
-            informasi: {
-                nama_kereta: $('table:nth-child(1) tr:nth-child(6) td:nth-child(3) span').html(),
-                kelas: $('table:nth-child(1) tr:nth-child(7) td:nth-child(3)').html(),
-                asal: $('table:nth-child(1) tr:nth-child(8) td:nth-child(3)').html(),
-                tujuan: $('table:nth-child(1) tr:nth-child(9) td:nth-child(3)').html(),
-                berangkat: $('table:nth-child(1) tr:nth-child(10) td:nth-child(3)').text(),
-                datang: $('table:nth-child(1) tr:nth-child(11) td:nth-child(3)').text(),
-            },
-            penumpang: this.passenger.toArray()
+            perjalanan: {
+                nama_kereta     : $($('.row:not(".head")')[1]).find(".row .columns:nth-child(1) dl dd").text(),
+                kelas           : $($('.row:not(".head")')[1]).find(".row .columns:nth-child(2) dl dd").text(),
+                berangkat       : $($('.row:not(".head")')[1]).find(".row .columns:nth-child(3) dl dd").text(),
+                tiba            : $($('.row:not(".head")')[1]).find(".row .columns:nth-child(4) dl dd").text(),  
+            }
         }
-    }
+    },
+    parsePassenger(){
+        const $    = this.html
+        let data   = $(".row:not('.head')")
+        let length = data.length
+        let start  = 2
+        let end    = length - ( 2 - 1 )
 
+        var res = [];
+        for ( i = start; i <= end; i++ ) {
+            res.push({
+                nama:               $(data[i]).find(".row .columns:nth-child(1) dl dd").text(),
+                id_number:          $(data[i]).find(".row .columns:nth-child(2) dl dd").text(),
+                tipe:               $(data[i]).find(".row .columns:nth-child(3) dl dd").text(),
+                tempat_duduk:       $(data[i]).find(".row .columns:nth-child(4) dl dd").text(),
+                no_tiket:           $(data[i]).find(".row .columns:nth-child(5) dl dd").text(),
+            })
+        }
+
+        this.passenger = res
+    },
+    output(){
+        let output =  {
+            general: this.general,
+            passenger: this.passenger
+        }
+
+        spinner.succeed(" done ")
+
+        var tableGeneral = new Table({});
+
+        tableGeneral.push(
+            [chalk.red("Status Bayar"), chalk.green(this.general.pembayaran.status_bayar)],
+            [chalk.red("Total Bayar") , chalk.yellow(this.general.pembayaran.total_bayar)],
+            [chalk.red("Nama Kereta") , this.general.perjalanan.nama_kereta],
+            [chalk.red("Kelas")       , this.general.perjalanan.kelas],
+            [chalk.red("Berangkat")   , this.general.perjalanan.berangkat],
+            [chalk.red("Tiba")        , this.general.perjalanan.tiba]
+        )
+        console.log( tableGeneral.toString() )
+        
+        var tablePassenger = new Table({
+            head: ['No', 'Nama', 'NO ID', 'Tipe', 'Tempat Duduk', 'Tiket']
+        });
+        
+        this.passenger.map(( item, index ) => {
+            tablePassenger.push(
+                [ chalk.green(index+1) , item.nama , chalk.blue(item.id_number), item.tipe, item.tempat_duduk, item.no_tiket]
+            );
+        })
+        console.log(tablePassenger.toString());
+    }
 }
 
 
